764b87b3bb1958a8daf0d9526d1ae6756940a323
ysftzcn <yusuftezcan@gmail.com>
2020-12-31 13:37:57 +0300

    vuepress start

---
+++ portfolio/.vuepress/dist/assets/js/6.48ea2a22.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{303:function(t,n,s){},311:function(t,n,s){"use strict";s(303)},319:function(t,n,s){"use strict";s.r(n);var e={},a=(s(311),s(38)),c=Object(a.a)(e,(function(){var t=this.$createElement;return(this._self._c||t)("span",{staticClass:"label"},[this._t("default")],2)}),[],!1,null,"3b34421d",null);n.default=c.exports}}]);
--- portfolio/index.md
+++ portfolio/index.md
@@ -1,6 +1,6 @@
 ---
 works_index: true
-hero_text: "<strong>ysftzcn - ysftzcn </strong>, we work for brands. But most importantly, we work for fun."
+hero_text: "<strong>ysftzcn</strong>, we work for brands. But most importantly, we work for fun."
 title: Hero
 
 ---
